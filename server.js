7a0741e943bac2a0885758ff664a20e3306a13ca
Mawaddah <itzolda@gmail.com>
2020-09-24 14:15:39 +0100

    Challenge one completed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 const welcomeMessage = {
@@ -17,7 +17,37 @@ const welcomeMessage = {
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.send("Welcome to chatterbox!");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function (request, response) {
+  response.send(messages);
+});
+
+app.post("/messages", function (req, res) {
+  const newMessage = req.body;
+  console.log(newMessage);
+  messages.push(newMessage);
+  res.send(newMessage);
+});
+
+app.get("/messages/:messageId", function (req, res) {
+  const theMessageId = parseInt(req.params.messageId);
+  console.log(typeof theMessageId);
+  const findMessage = messages.find((message) => message.id === theMessageId);
+  res.send(findMessage);
+});
+
+app.delete("/messages/:messageId", function (req, res) {
+  const theMessageId = parseInt(req.params.messageId);
+  const messageToDelete = messages.find(
+    (message) => message.id === theMessageId
+  );
+  console.log(messageToDelete);
+  const indexOfMessage = messages.indexOf(messageToDelete);
+  messages.splice(indexOfMessage, 1);
+  res.send(messageToDelete);
+});
+app.listen(3000, () => {
+  console.log("I'm listening");
+});
